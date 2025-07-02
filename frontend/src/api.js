b0cd4f5ff295eef46adc13543f269e4f6f21e322
FixxooXD <rajpalmal40@gmail.com>
2025-07-02 22:54:12 +0530

    add dynamic baseURL for axios based on environment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const isProd = import.meta.env.VITE_PROD;
+console.log("Production mode:", isProd);
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000/",
+  baseURL: isProd
+    ? "https://taskmanger-flaskapp.onrender.com"
+    : "http://127.0.0.1:5000/",
 });
 
 export default API;
